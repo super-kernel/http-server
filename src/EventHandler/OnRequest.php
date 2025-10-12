eaf45eee94708d936a7d852ac6b93bcdedbac598
root <wheakerd@gmail.com>
2025-10-12 11:58:55 +0800

    fix: Fix the wrapper calling problem.

--- src/EventHandler/OnRequest.php
+++ src/EventHandler/OnRequest.php
@@ -3,14 +3,11 @@ declare(strict_types=1);
 
 namespace SuperKernel\HttpServer\EventHandler;
 
-use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\ResponseInterface;
-use SuperKernel\Context\Context;
 use SuperKernel\HttpServer\Context\RequestContext\RequestContext;
 use SuperKernel\HttpServer\Context\RequestContext\ResponseContext;
 use SuperKernel\HttpServer\Factory\RouteDispatcher;
-use SuperKernel\HttpServer\Provider\RequestWrapper;
-use SuperKernel\HttpServer\Provider\ResponseWrapper;
+use SuperKernel\HttpServer\Wrapper\RequestWrapper;
+use SuperKernel\HttpServer\Wrapper\ResponseWrapper;
 use SuperKernel\Server\Attribute\Event;
 use SuperKernel\Server\Enumeration\HttpServerEvent;
 use Swoole\Http\Request;
